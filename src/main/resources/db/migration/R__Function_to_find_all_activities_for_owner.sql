9fa89f9292104f43543545202146a8c8df136624
Axel Marcon <axel.marcon@iut-rodez.fr>
2019-03-06 15:56:50 +0100

    GIT Fix 3.2 function to find all activities for a given owner

--- src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -1,1 +1,7 @@
-CREATE OR REPLACE FUNCTION find_all_activities_for_owner
+CREATE OR REPLACE FUNCTION find_all_activities_for_owner(username varchar(500)) RETURNS SETOF activity AS $$
+	SELECT activity.*
+	FROM "user"
+	JOIN activity
+	ON "user".id = activity.owner_id
+	WHERE  find_all_activities_for_owner.username = "user".username
+$$ LANGUAGE SQL;
